8b4d2aad06b386f8e1d736192939b99a95055ff4
Benjamin Chiu <894306909@qq.com>
2021-12-12 21:14:46 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-12 21:01:44
+ * @LastEditTime: 2021-12-12 21:01:45
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \console-nuxt\layouts\default.vue
+-->
+<template>
+  <div>
+    <div>我的博客导航栏在这里</div>
+    <nuxt />
+  </div>
+</template>
---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-12 20:51:36
+ * @LastEditTime: 2021-12-12 20:51:37
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \console-nuxt\pages\about\index.vue
+-->
+<template>
+  <Tutorial/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
